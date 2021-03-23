ab15b9b940da97b300211adb3c49b6d82ffe3dd1
Andre <andrebelcore@gmail.com>
2021-03-23 15:45:36 +0100

    Initial Commit

---
+++ FizzBuzzLight-year/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  FizzBuzzLight-year
+//
+//  Created by Andrea Belcore on 23/03/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
